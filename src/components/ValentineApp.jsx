534864e182aa2de62a53a7ebb54822e6afd9d52f
Banti Kumar Singh <bantikumarsingh91@gmail.com>
2026-02-18 20:04:00 +0530

    add for push

--- src/components/ValentineApp.jsx
+++ src/components/ValentineApp.jsx
@@ -43,6 +43,7 @@ export default function ValentineApp() {
                 marginLeft: '10px' 
               }}
             >
+            
               NO 😝
             </button>
           </div>
